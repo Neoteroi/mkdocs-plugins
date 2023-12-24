47da505e67d024692d1e9b9894a400ce06bb4377
Roberto Prevato <roberto.prevato@gmail.com>
2023-12-24 17:11:28 +0100

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -11,7 +11,7 @@ except ImportError:
     import pkg_resources
 
     def get_resource_file_path(file_name: str) -> str:
-        return pkg_resources.resource_filename("tests.res", file_name)
+        return pkg_resources.resource_filename(__name__, file_name)
 
 
 def get_resource_file_contents(file_name: str) -> str:
